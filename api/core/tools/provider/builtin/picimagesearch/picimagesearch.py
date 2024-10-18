214cc7e5cfa4a4753f1656b9fb081de41bd83d8b
kitUIN <kulujun@gmail.com>
2024-10-18 09:39:04 +0800

    fix: error import

--- api/core/tools/provider/builtin/picimagesearch/picimagesearch.py
+++ api/core/tools/provider/builtin/picimagesearch/picimagesearch.py
@@ -1,14 +1,9 @@
-from core.tools.entities.tool_entities import ToolInvokeMessage, ToolProviderType
-from core.tools.tool.tool import Tool
+
 from core.tools.provider.builtin_tool_provider import BuiltinToolProviderController
 from core.tools.errors import ToolProviderCredentialValidationError
 
-from core.tools.provider.builtin.google.tools.google_search import GoogleSearchTool
-
 from typing import Any, Dict
 
-from PicImageSearch import Network, SauceNAO
-from PicImageSearch.model import SauceNAOResponse
 
 class PicImageSearchProvider(BuiltinToolProviderController):
     def _validate_credentials(self, credentials: Dict[str, Any]) -> None:
