effcd8c2a14283ed24909701879befeefc46142e
kitUIN <kulujun@gmail.com>
2024-10-24 16:11:55 +0800

    logo

--- web/app/components/base/logo/logo-site.tsx
+++ web/app/components/base/logo/logo-site.tsx
@@ -16,7 +16,7 @@ const LogoSite: FC<LogoSiteProps> = ({
     }
   })
 
-  const src = theme === 'light' ? '/logo/logo-site.png' : `/logo/logo-site-${theme}.png`
+  const src = theme === 'light' ? '/dify/logo/logo-site.png' : `/dify/logo/logo-site-${theme}.png`
   return (
     <img
       src={src}
